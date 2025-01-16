0562b2f6e0426a506c4ff759f8fbc3309b661c98
24LP6042 24LP6042 <24LP6042@administrators-iMac.local>
2025-01-16 15:54:34 +0800

    second

---
+++ index.js
@@ -0,0 +1,26 @@
+function niilber(a,b) {
+    result = a + b
+    return result
+};
+niilber(2,5)
+
+function ylgavar(a,b) {
+    ylgav = a - b
+    return ylgav
+};
+ylgavar(2,5)
+
+function urjih(a,b) {
+    urj = a * b
+    return urj
+};
+urjih(2,5)
+
+function huwaah(a,b) {
+    huwaa = a / b
+    return huwaa
+};
+huwaah(2,5)
+
+console.log(niilber(2,5), ylgavar(2,5), urjih(2,5), huwaah(2,5));
+
